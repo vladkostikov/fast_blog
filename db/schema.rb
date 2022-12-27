6f338824fb3a35cbaebb142b283d21a487b6263d
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-27 23:54:07 +0300

    Сделал миграцию Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_202226) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_204734) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
